5c349653b0a67b546625768d9fd2c576aaf474b2
k33asby <k33asby@gmail.com>
2018-06-09 00:57:36 +0900

    Add NOT NULL to migration

--- db/migrate/20180608154019_create_users.rb
+++ db/migrate/20180608154019_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string  :name
-      t.integer :age
-      t.integer :sex
-      t.string  :email
-      t.string  :password_digest
-      t.string  :picture
+      t.string  :name,            :null => false
+      t.integer :age,             :null => false
+      t.integer :sex,             :null => false
+      t.string  :email,           :null => false
+      t.string  :password_digest, :null => false
+      t.string  :picture,         :null => false
       t.timestamps
     end
   end
--- db/migrate/20180608154025_create_items.rb
+++ db/migrate/20180608154025_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
-      t.string  :name
-      t.integer :price
-      t.string :picture
+      t.string  :name,    :null => false
+      t.integer :price,   :null => false
+      t.string :picture,  :null => false
       t.timestamps
     end
   end
