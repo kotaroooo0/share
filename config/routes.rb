ecf860fc85f01c8ba3181edaa594ec635ea2735c
k33asby <k33asby@gmail.com>
2018-06-09 12:41:12 +0900

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-
   root 'items#index'
-  resources :users
+  resources :users, only: %i[index show edit update destroy]
   resources :items
-  resources :sessions, only: %i[new create destroy]
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get   '/login',   to: 'sessions#new'
+  post  '/login',   to: 'sessions#create'
+  delete'/logout',  to: 'sessions#destroy'
 end
