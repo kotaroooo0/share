6367d99fcca9034cf66bf28192c3f4cb87d7ede8
k33asby <k33asby@gmail.com>
2018-06-19 16:01:32 +0900

    Add exhibit feature

---
+++ app/controllers/user_items_controller.rb
@@ -0,0 +1,18 @@
+class UserItemsController < ApplicationController
+
+  def create
+    @item = Item.find(params[:user_item][:item_id])
+    @exhibit = UserItem.new(user_item_params)
+    if @exhibit.save
+      flash[:success] = '出品しました'
+    end
+    redirect_to @item
+  end
+
+  private
+
+  def user_item_params
+    params.require(:user_item).permit(:user_id, :item_id, :price, :condition)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :users, only: %i[show edit update destroy]
   resources :items
+  resources :user_items, only: %i[create]
 
   get '/search', to: 'items#search'
   get '/signup', to: 'users#new'
