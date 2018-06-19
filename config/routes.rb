a0b1c23b931913e27e93f9dc6010f179b37d03a6
k33asby <k33asby@gmail.com>
2018-06-19 20:59:02 +0900

    Add logout

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get   '/login',   to: 'sessions#new'
   post  '/login',   to: 'sessions#create'
-  delete'/logout',  to: 'sessions#destroy'
+  get '/logout',  to: 'sessions#destroy'
 end
