8aa42a342a6680394c4f9b31505de97de736660b
k33asby <k33asby@gmail.com>
2018-06-21 18:07:02 +0900

    Solve N+1 query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @exhibits = UserItem.where(user_id: params[:id])
+    @exhibits = UserItem.where(user_id: params[:id]).includes(:item)
   end
 
   def new
