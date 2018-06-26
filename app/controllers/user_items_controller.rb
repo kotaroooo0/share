ccee98a55d33afc05e7028052f3a1faf44f14780
k33asby <k33asby@gmail.com>
2018-06-26 23:35:07 +0900

    Add item info and solve N+1

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -3,7 +3,7 @@ class UserItemsController < ApplicationController
   before_action :logged_in_user?, only: %i[create]
 
   def index
-    @exhibits = UserItem.order('created_at DESC').limit(9)
+    @exhibits = UserItem.order('created_at DESC').limit(9).includes(:user).includes(:item)
 
   end
 
