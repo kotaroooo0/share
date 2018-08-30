92bf892c3a75c447bfb3f831215c63aba207546a
k33asby <k33asby@gmail.com>
2018-08-31 00:40:55 +0900

    アノテーションを追加

--- spec/factories/applies.rb
+++ spec/factories/applies.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: applies
+#
+#  id            :bigint(8)        not null, primary key
+#  user_id       :bigint(8)        not null
+#  exhibition_id :bigint(8)        not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 FactoryBot.define do
   factory :apply do
     
--- spec/models/apply_spec.rb
+++ spec/models/apply_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: applies
+#
+#  id            :bigint(8)        not null, primary key
+#  user_id       :bigint(8)        not null
+#  exhibition_id :bigint(8)        not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Apply, type: :model do
